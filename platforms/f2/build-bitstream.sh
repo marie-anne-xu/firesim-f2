799a0347ca94ac080185db7739371fa38ee36e0b
marie-anne-xu <marieanne.xu@berkeley.edu>
2026-03-03 02:52:26 +0000

    feat: xdma bitstream flow complete

--- platforms/f2/build-bitstream.sh
+++ platforms/f2/build-bitstream.sh
@@ -73,4 +73,4 @@ cd $CL_DIR/build/scripts
 # ./aws_build_dcp_from_cl.sh  -strategy $STRATEGY -frequency $FREQUENCY -foreground
 export CL_NAME=$(basename $CL_DIR)
 
-./aws_build_dcp_from_cl.py -c $CL_NAME --frequency $FREQUENCY --aws_clk_gen --clock_recipe_a A1  --clock_recipe_b B0 --clock_recipe_c C0 --mode small_shell
+./aws_build_dcp_from_cl.py -c $CL_NAME --frequency $FREQUENCY --aws_clk_gen --clock_recipe_a A1  --clock_recipe_b B0 --clock_recipe_c C0 --mode xdma_shell
